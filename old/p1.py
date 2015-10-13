21fba236f975db488e70c4a426bf2e1def3f39eb
Radu Ciorba <radu@devrandom.ro>
2015-10-13 15:44:35 +0300

    review p1

--- old/p1.py
+++ old/p1.py
@@ -7,17 +7,27 @@
 # single line.
 
 
+# to nitpick: min and max are builtin functions, and you should avoid using these names for variables
 def numbers(min, max):
-    result = []
-
-    for number in xrange(min, max):
+    # result = []
+    # for number in xrange(min, max):
+    # you want to go to max+1, xrange(1, 3) gives you [1, 2]
+    for number in xrange(min, max + 1):
         if number % 7 == 0 and number % 5 is not 0:
-            result.append(number)
-    return result
+            # result.append(number)
+            yield number  # if you use yield you get a generator, and we don't need to build
+            # up the list in memory; think of it as a sort of lazy list
+            # yield is like having a function "return" a value then continue to execute
+    # return result
 
 
-printed_result = ""
-for number in  numbers(2000, 3200):
-    printed_result = "{} {},".format(printed_result, number)
-print printed_result[:-1]
+# printed_result = ""
+# for number in  numbers(2000, 3200):
+#     printed_result = "{} {},".format(printed_result, number)
+# print printed_result[:-1]
 
+# just use the join method of the string type, pass it a list of strings and no
+# more trailing comma to handle
+print ",".join(
+    [str(num) for num in numbers(2000, 3200)]
+)
